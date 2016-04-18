e201dacec58e8a118035779f4d25fb5b00e2a223
Andreas Zwinkau <qznc@web.de>
2016-04-18 19:57:51 +0200

    reorder and other documentation improvements

--- source/money.d
+++ source/money.d
@@ -1,172 +1,15 @@
+/******
+ * Handling money amounts safely and efficiently.
+ *
+ * License: $(LINK2 http://www.boost.org/LICENSE_1_0.txt, Boost License 1.0)
+ * Authors: Andreas Zwinkau
+ */
+module money;
+
 import std.math : pow, floor, ceil, lrint, abs;
 import std.conv : to;
 import core.checkedint : adds, subs;
 import std.format : FormatSpec, formattedWrite;
-
-/** Specifies rounding behavior
-
-Inspired by java.math.RoundingMode.
-**/
-enum roundingMode {
-    /** Round upwards */
-    UP,
-    /** Round downwards */
-    DOWN,
-    /** See UP */
-    CEILING,
-    /** See DOWN */
-    FLOOR,
-    /** See DOWN */
-    TRUNC,
-    /** Round to nearest number, half way between round up */
-    HALF_UP,
-    /** Round to nearest number, half way between round down */
-    HALF_DOWN,
-    /** Round to nearest number, half way between round to even number */
-    HALF_EVEN,
-    /** Round to nearest number, half way between round to odd number */
-    HALF_ODD,
-    /** Round to nearest number, half way between round towards zero  */
-    HALF_TO_ZERO,
-    /** Round to nearest number, half way between round away from zero  */
-    HALF_FROM_ZERO,
-    /** Error if rounding would be necessary */
-    UNNECESSARY
-}
-
-/** Round an integer to a certain decimal place according to rounding mode */
-long round(roundingMode m)(long x, int dec_place)
-out (result) {
-    assert ((result % pow(10, dec_place)) == 0);
-}
-body {
-    const zeros = pow(10, dec_place);
-    /* short cut, also removes edge cases */
-    if ((x % zeros) == 0)
-        return x;
-
-    const half  = zeros / 2;
-    with (roundingMode) {
-        static if (m == CEILING || m == UP) {
-            return ((x / zeros) + 1) * zeros;
-        } else static if (m == FLOOR || m == TRUNC || m == DOWN) {
-            return x / zeros * zeros;
-        } else static if (m == HALF_UP) {
-            if ((x % zeros) >= half)
-                return ((x / zeros) + 1) * zeros;
-            else
-                return x / zeros * zeros;
-        } else static if (m == HALF_DOWN) {
-            if ((x % zeros) > half)
-                return ((x / zeros) + 1) * zeros;
-            else
-                return x / zeros * zeros;
-        } else static if (m == HALF_EVEN) {
-            const down = x / zeros;
-            if (down % 2 == 0)
-                return down * zeros;
-            else
-                return (down+1) * zeros;
-        } else static if (m == HALF_ODD) {
-            const down = x / zeros;
-            if (down % 2 == 0)
-                return (down+1) * zeros;
-            else
-                return down * zeros;
-        } else static if (m == HALF_TO_ZERO) {
-            const down = x / zeros;
-            if (down < 0) {
-                if (abs(x % zeros) <= half) {
-                    return (down) * zeros;
-                } else {
-                    return (down-1) * zeros;
-                }
-            } else {
-                if ((x % zeros) > half) {
-                    return (down+1) * zeros;
-                } else {
-                    return (down) * zeros;
-                }
-            }
-        } else static if (m == HALF_FROM_ZERO) {
-            const down = x / zeros;
-            if (down < 0) {
-                if (abs(x % zeros) < half) {
-                    return (down) * zeros;
-                } else {
-                    return (down-1) * zeros;
-                }
-            } else {
-                if (x % zeros >= half) {
-                    return (down+1) * zeros;
-                } else {
-                    return (down) * zeros;
-                }
-            }
-        } else static if (m == UNNECESSARY) {
-            throw new ForbiddenRounding();
-        }
-    }
-}
-
-///
-unittest {
-    assert (round!(roundingMode.DOWN)     (1001, 1) == 1000);
-    assert (round!(roundingMode.UP)       (1001, 1) == 1010);
-    assert (round!(roundingMode.HALF_UP)  (1005, 1) == 1010);
-    assert (round!(roundingMode.HALF_DOWN)(1005, 1) == 1000);
-}
-
-@safe pure @nogc nothrow
-unittest {
-    assert (round!(roundingMode.HALF_UP)       ( 10, 1) ==  10);
-    assert (round!(roundingMode.CEILING)       ( 11, 1) ==  20);
-    assert (round!(roundingMode.FLOOR)         ( 19, 1) ==  10);
-    assert (round!(roundingMode.TRUNC)         ( 19, 1) ==  10);
-    assert (round!(roundingMode.HALF_UP)       ( 15, 1) ==  20);
-    assert (round!(roundingMode.HALF_UP)       (-15, 1) == -10);
-    assert (round!(roundingMode.HALF_DOWN)     ( 15, 1) ==  10);
-    assert (round!(roundingMode.HALF_DOWN)     ( 16, 1) ==  20);
-    assert (round!(roundingMode.HALF_EVEN)     ( 15, 1) ==  20);
-    assert (round!(roundingMode.HALF_EVEN)     ( 25, 1) ==  20);
-    assert (round!(roundingMode.HALF_ODD)      ( 15, 1) ==  10);
-    assert (round!(roundingMode.HALF_ODD)      ( 25, 1) ==  30);
-    assert (round!(roundingMode.HALF_TO_ZERO)  ( 25, 1) ==  20);
-    assert (round!(roundingMode.HALF_TO_ZERO)  ( 26, 1) ==  30);
-    assert (round!(roundingMode.HALF_TO_ZERO)  (-25, 1) == -20);
-    assert (round!(roundingMode.HALF_TO_ZERO)  (-26, 1) == -30);
-    assert (round!(roundingMode.HALF_FROM_ZERO)( 25, 1) ==  30);
-    assert (round!(roundingMode.HALF_FROM_ZERO)( 24, 1) ==  20);
-    assert (round!(roundingMode.HALF_FROM_ZERO)(-25, 1) == -30);
-    assert (round!(roundingMode.HALF_FROM_ZERO)(-24, 1) == -20);
-}
-
-unittest {
-    import std.exception : assertThrown;
-    assert (round!(roundingMode.UNNECESSARY)   ( 10, 1) ==  10);
-    assertThrown!ForbiddenRounding(round!(roundingMode.UNNECESSARY)(12, 1) == 10);
-}
-
-/** Round a float to an integer according to rounding mode */
-//pure nothrow @nogc @trusted
-real round(real x, roundingMode m)
-body {
-    final switch (m) with (roundingMode) {
-        case CEILING: goto case;
-        case UP: return ceil(x);
-        case FLOOR: goto case;
-        case DOWN: return floor(x);
-        case HALF_UP: return lrint(x);
-        case HALF_DOWN: return lrint(x);
-        case HALF_EVEN: return lrint(x);
-        case HALF_ODD: return x; // FIXME
-        case TRUNC: goto case;
-        case HALF_TO_ZERO: return x; // FIXME
-        case HALF_FROM_ZERO: return x; // FIXME
-        case UNNECESSARY:
-            throw new ForbiddenRounding();
-    }
-}
 
 /** Holds an amount of money **/
 struct money(string curr, int dec_places = 4, roundingMode rmode = roundingMode.HALF_UP) {
@@ -183,9 +26,13 @@ struct money(string curr, int dec_places = 4, roundingMode rmode = roundingMode.
         return ret;
     }
 
+    /// default initialisation value
     static immutable init = fromLong(0L);
+    /// maximum amount
     static immutable max = fromLong(long.max);
+    /// minimum amount
     static immutable min = fromLong(long.min);
+
     private static immutable dec_mask = to!long(pow(10.0, dec_places));
 
     T opBinary(string op)(const T rhs) const
@@ -255,6 +102,159 @@ unittest {
     auto one = EUR(1);
     assertThrown!OverflowException(m + one);
     assertThrown!OverflowException(EUR.min - one);
+}
+
+
+/** Specifies rounding behavior **/
+enum roundingMode {
+    /** Round upwards, e.g. 3.1 up to 4. */
+    UP,
+    /** Round downwards, e.g. 3.9 down to 3. */
+    DOWN,
+    /** Round to nearest number, half way between round up, e.g. 3.5 to 4. */
+    HALF_UP,
+    /** Round to nearest number, half way between round dow, e.g. 3.5 to 3.  */
+    HALF_DOWN,
+    /** Round to nearest number, half way between round to even number, e.g. 3.5 to 4. */
+    HALF_EVEN,
+    /** Round to nearest number, half way between round to odd number, e.g. 3.5 to 3. */
+    HALF_ODD,
+    /** Round to nearest number, half way between round towards zero, e.g. -3.5 to -3.  */
+    HALF_TO_ZERO,
+    /** Round to nearest number, half way between round away from zero, e.g. -3.5 to -4.  */
+    HALF_FROM_ZERO,
+    /** Throw exception if rounding would be necessary */
+    UNNECESSARY
+}
+
+/** Round an integer to a certain decimal place according to rounding mode */
+long round(roundingMode m)(long x, int dec_place)
+out (result) {
+    assert ((result % pow(10, dec_place)) == 0);
+}
+body {
+    const zeros = pow(10, dec_place);
+    /* short cut, also removes edge cases */
+    if ((x % zeros) == 0)
+        return x;
+
+    const half  = zeros / 2;
+    with (roundingMode) {
+        static if (m == UP) {
+            return ((x / zeros) + 1) * zeros;
+        } else static if (m == DOWN) {
+            return x / zeros * zeros;
+        } else static if (m == HALF_UP) {
+            if ((x % zeros) >= half)
+                return ((x / zeros) + 1) * zeros;
+            else
+                return x / zeros * zeros;
+        } else static if (m == HALF_DOWN) {
+            if ((x % zeros) > half)
+                return ((x / zeros) + 1) * zeros;
+            else
+                return x / zeros * zeros;
+        } else static if (m == HALF_EVEN) {
+            const down = x / zeros;
+            if (down % 2 == 0)
+                return down * zeros;
+            else
+                return (down+1) * zeros;
+        } else static if (m == HALF_ODD) {
+            const down = x / zeros;
+            if (down % 2 == 0)
+                return (down+1) * zeros;
+            else
+                return down * zeros;
+        } else static if (m == HALF_TO_ZERO) {
+            const down = x / zeros;
+            if (down < 0) {
+                if (abs(x % zeros) <= half) {
+                    return (down) * zeros;
+                } else {
+                    return (down-1) * zeros;
+                }
+            } else {
+                if ((x % zeros) > half) {
+                    return (down+1) * zeros;
+                } else {
+                    return (down) * zeros;
+                }
+            }
+        } else static if (m == HALF_FROM_ZERO) {
+            const down = x / zeros;
+            if (down < 0) {
+                if (abs(x % zeros) < half) {
+                    return (down) * zeros;
+                } else {
+                    return (down-1) * zeros;
+                }
+            } else {
+                if (x % zeros >= half) {
+                    return (down+1) * zeros;
+                } else {
+                    return (down) * zeros;
+                }
+            }
+        } else static if (m == UNNECESSARY) {
+            throw new ForbiddenRounding();
+        }
+    }
+}
+
+///
+unittest {
+    assert (round!(roundingMode.DOWN)     (1009, 1) == 1000);
+    assert (round!(roundingMode.UP)       (1001, 1) == 1010);
+    assert (round!(roundingMode.HALF_UP)  (1005, 1) == 1010);
+    assert (round!(roundingMode.HALF_DOWN)(1005, 1) == 1000);
+}
+
+@safe pure @nogc nothrow
+unittest {
+    assert (round!(roundingMode.HALF_UP)       ( 10, 1) ==  10);
+    assert (round!(roundingMode.UP)            ( 11, 1) ==  20);
+    assert (round!(roundingMode.DOWN)          ( 19, 1) ==  10);
+    assert (round!(roundingMode.HALF_UP)       ( 15, 1) ==  20);
+    assert (round!(roundingMode.HALF_UP)       (-15, 1) == -10);
+    assert (round!(roundingMode.HALF_DOWN)     ( 15, 1) ==  10);
+    assert (round!(roundingMode.HALF_DOWN)     ( 16, 1) ==  20);
+    assert (round!(roundingMode.HALF_EVEN)     ( 15, 1) ==  20);
+    assert (round!(roundingMode.HALF_EVEN)     ( 25, 1) ==  20);
+    assert (round!(roundingMode.HALF_ODD)      ( 15, 1) ==  10);
+    assert (round!(roundingMode.HALF_ODD)      ( 25, 1) ==  30);
+    assert (round!(roundingMode.HALF_TO_ZERO)  ( 25, 1) ==  20);
+    assert (round!(roundingMode.HALF_TO_ZERO)  ( 26, 1) ==  30);
+    assert (round!(roundingMode.HALF_TO_ZERO)  (-25, 1) == -20);
+    assert (round!(roundingMode.HALF_TO_ZERO)  (-26, 1) == -30);
+    assert (round!(roundingMode.HALF_FROM_ZERO)( 25, 1) ==  30);
+    assert (round!(roundingMode.HALF_FROM_ZERO)( 24, 1) ==  20);
+    assert (round!(roundingMode.HALF_FROM_ZERO)(-25, 1) == -30);
+    assert (round!(roundingMode.HALF_FROM_ZERO)(-24, 1) == -20);
+}
+
+unittest {
+    import std.exception : assertThrown;
+    assert (round!(roundingMode.UNNECESSARY)   ( 10, 1) ==  10);
+    assertThrown!ForbiddenRounding(round!(roundingMode.UNNECESSARY)(12, 1) == 10);
+}
+
+/** Round a float to an integer according to rounding mode */
+//pure nothrow @nogc @trusted
+real round(real x, roundingMode m)
+body {
+    final switch (m) with (roundingMode) {
+        case UP: return ceil(x);
+        case DOWN: return floor(x);
+        case HALF_UP: return lrint(x);
+        case HALF_DOWN: return lrint(x);
+        case HALF_EVEN: return lrint(x);
+        case HALF_ODD: return x; // FIXME
+        case HALF_TO_ZERO: return x; // FIXME
+        case HALF_FROM_ZERO: return x; // FIXME
+        case UNNECESSARY:
+            throw new ForbiddenRounding();
+    }
 }
 
 class ForbiddenRounding : Exception {
